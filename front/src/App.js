32496f4318bf0b6b47c00f736bbf5a309662923f
Henna <henna.haapala@gmail.com>
2021-12-15 16:30:36 +0200

    front ja zip juukeli

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,10 @@
-import React from 'react'
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => { 
+  return(
+      <div>Hello World!!</div>
+    )
+  }
 
 export default App
